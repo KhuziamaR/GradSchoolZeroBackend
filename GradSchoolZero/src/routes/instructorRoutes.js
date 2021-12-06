45ee8945d0babce72d5e3ebcbf34d59f51630821
John Jakobsen <jerikjakobsen@gmail.com>
2021-12-05 20:30:25 -0500

    Assign grade now updates students GPA, getStudentsForCourse now returns a list of students

--- GradSchoolZero/src/routes/instructorRoutes.js
+++ GradSchoolZero/src/routes/instructorRoutes.js
@@ -51,35 +51,90 @@ const getStudentsForCourse = (req, res) => {
 	req.db
 		.query(`SELECT studentid FROM class WHERE courseid = '${courseid}';`)
 		.then((data) => {
-			res.status(200).send({
-				data: data.rows
-			});
+			if (data.rowCount == 0) {
+                return res.status(200).send({students: []});
+            }
+            req.db.query(`SELECT * from student WHERE ${buildOrStudentList("", data.rows)};`)
+            .then(data => {
+                return res.status(200).send({students: data.rows});
+            })
+            .catch((error) => {
+                console.log(error)
+                res.status(500).send({
+                    msg: 'An error occurred.'
+                });
+            });
 		})
 		.catch((error) => {
-			res.status(404).send({
-				msg: 'Error while retrieving students for course.'
+            console.log(error)
+			res.status(500).send({
+				msg: 'An error occurred.'
 			});
 		});
 };
 
 const assignGrade = (req, res) => {
 	const { studentid, courseid, grade } = req.query;
+    const grades = {
+        "A+": 4.0,
+        "A": 4.0,
+        "A-": 3.7,
+        "B+": 3.3,
+        "B": 3.0,
+        "B-": 2.7,
+        "C+": 2.3,
+        "C": 2.0,
+        "C-": 1.7,
+        "D+": 1.3,
+        "D": 1.0,
+        "F": 0 
+    }
 
 	if (!studentid || !courseid || !grade) return res.status(500).send({ msg: 'Send all inputs' });
 
 	req.db
 		.query(`UPDATE class SET grade = '${grade}' WHERE studentid = '${studentid}' AND courseid = '${courseid}';`)
 		.then((data) => {
-			if (data.rowCount) {
-				res.status(200).send({ msg: 'Success!' });
-			} else {
-				res.status(500).send({ msg: 'Error' });
-			}
+            if (data.rowCount == 0) {
+                res.status(500).send({ msg: 'Error' });
+            }
+            req.db.query(`SELECT * FROM class WHERE studentid = '${studentid}' AND NOT grade='';`)
+            .then(data => {
+                const studentGrades = data.rows.map((val) => {
+                    return grades[val.grade];
+                })
+                const sum = (list) => {
+                    if (list.length == 0) return 0;
+                    return list[0] + sum(list.slice(1, list.length));
+                }
+                const avg = sum(studentGrades)/ studentGrades.length;
+                req.db.query(`UPDATE student set gpa = ${avg} WHERE id = '${studentid}';`)
+                .then(data2 => {
+                    if (data2.rowCount) {
+                        return res.status(200).send({msg: "Success!"});
+                    }
+                    return res.status(500).send({msg: "Error!"})
+                })
+                .catch((error) => {
+                    console.log(error);
+                    res.status(500).send({ msg: 'Something went wrong.' });
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+                res.status(500).send({ msg: 'Something went wrong.' });
+            });
 		})
 		.catch((error) => {
 			console.log(error);
 			res.status(500).send({ msg: 'Something went wrong.' });
 		});
+};
+
+const buildOrStudentList = (result, studentids) => {
+	if (studentids.length == 0) return result;
+	if (studentids.length == 1) return result + `id = '${studentids[0].studentid}'`;
+	return buildOrStudentList(result + `id = '${studentids[0].studentid}' OR `, studentids.slice(1, studentids.length));
 };
 
 module.exports = {
